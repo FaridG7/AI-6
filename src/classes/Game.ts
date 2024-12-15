63d97352753de0460886085124b8001529b67c66
Farid <goharifarid7@gmail.com>
2024-12-15 13:53:00 +0330

    add min, max & ...

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -97,43 +97,69 @@ export class Game {
     return this.uncapturedTiles === 0;
   }
 
-  private calculateBestAction(): Action {}
+  private calculateBestAction(player: 1 | 2): Action {
+    return Game.max(player, this, 3).action;
+  }
 
-  private static bestAction(
+  private static max(
     player: 1 | 2,
     state: Game,
-    allowedDepth: number
-    // alpha: number | "minusInfinit" = "minusInfinit",
-    // beta: number | "plusInfinit" = "plusInfinit"
+    allowedDepth: number,
+    alpha: number | "minusInfinit" = "minusInfinit",
+    beta: number | "plusInfinit" = "plusInfinit"
   ): { action: Action | null; value: number } {
-    const rival = player === 1 ? 2 : 1;
-
     if (state.isTerminal())
-      return { action: null, value: state[`player${player}`].score };
+      return {
+        action: null,
+        value: state[`player${player}`].score,
+      };
 
     if (allowedDepth < 0) return { action: null, value: 2 }; //TODO: needs heuristic value
 
     const possibleActions = state.getPossibleActions(player);
-    let bestAction: { action: Action | null; value: number } = null;
-    possibleActions.forEach((action) => {
-      const copiedState = state.copy();
-      copiedState.move(player, action);
-      const bestRivalAction = Game.bestAction(
-        rival,
-        copiedState,
-        allowedDepth - 1
-      );
-      if (bestAction && bestAction.value < bestRivalAction.value) {
-        bestAction = bestRivalAction;
-      } else {
-        bestAction = bestRivalAction;
-      }
+
+    const branches = possibleActions.map((possibleAction) => {
+      const tempState = state.copy();
+      tempState.move(player, possibleAction);
+      return Game.min(player, tempState, allowedDepth - 1);
     });
+
+    return branches.reduce((pre, cur) => (pre.value > cur.value ? pre : cur));
   }
 
+  private static min(
+    player: 1 | 2,
+    state: Game,
+    allowedDepth: number,
+    alpha: number | "minusInfinit" = "minusInfinit",
+    beta: number | "plusInfinit" = "plusInfinit"
+  ): { action: Action | null; value: number } {
+    if (state.isTerminal())
+      return {
+        action: null,
+        value: state[`player${player}`].score,
+      };
+
+    if (allowedDepth < 0) return { action: null, value: 2 }; //TODO: needs heuristic value(it should be calculated based on mode too)
+
+    const possibleActions = state.getPossibleActions(player);
+
+    const branches = possibleActions.map((possibleAction) => {
+      const tempState = state.copy();
+      tempState.move(player, possibleAction);
+      return Game.max(player, tempState, allowedDepth - 1);
+    });
+
+    return branches.reduce((pre, cur) => (pre.value < cur.value ? pre : cur));
+  }
   performAction(action: Action) {
     this.move(1, action);
-    // this.move(2,)
+    const bestP2Action = this.calculateBestAction(2);
+    if (bestP2Action) this.move(2, bestP2Action);
+    else
+      throw new Error(
+        "performAction: trying to perform an action while the game is finished"
+      );
   }
   getState() {}
   isFinished() {
